09af5e6e6881f1c86012620032355c3826f493f7
feizhaojun <m@feizhaojun.com>
2023-08-23 01:26:00 +0800

    update

---
+++ hello.c
@@ -0,0 +1,12 @@
+/*
+ * @Author: Mukti
+ * @Date: 2021-07-20 22:48:30
+ * @LastEditTime: 2021-07-20 23:24:40
+ * @LastEditors: Mukti
+ */
+#include<stdio.h>
+
+int main(){
+    printf("hello world!\n");
+    return 0;
+}
